566f882e13a8849d32d75d8e4e10e44d50744685
Hashim khan <hashimkhanlazak0301@gmail.com>
2025-10-03 23:23:26 +0500

    today i have checked all method of array

--- front2.js
+++ front2.js
@@ -1,7 +1,7 @@
 let array = [12,434,44,22,33];
 console.log(array.length);
 console.log("______---___")
-console.log(array.atindex[1])
+// console.log(array.atIndex[1])
 console.log("--____---___")
 console.log(array[7]);
 array[2] = 23; //assign new value 
@@ -75,3 +75,50 @@ for(let ch in char){
   console.log(ch, "appear", char[ch], "time");
 
 }
+// today task
+// Store your age in a variable. How do you check if you are a child, teenager, or adult?
+
+let age  = prompt("enter your age:");
+let age2 = parseInt(age);
+if(age<18){
+  console.log("you are child");
+}
+else if(age==18){
+  console.log("you are teenager");
+}
+else if(age>18){
+  console.log("you are adult");
+}
+//for each loop
+let arr = [12,34,23,45,66,45];
+arr.forEach((value)=> {
+  console.log(value);
+})
+///for of loop on array
+let nom_array = [12, 56,388,44,3399];
+for(let i of nom_array){
+  console.log(i);
+}
+console.log(nom_array);
+
+//for in loop
+let nam_array = [56,90,67,877,55,543];
+for(let num in nam_array){
+  console.log(num,":",nam_array);
+}
+console.log("----===___===----");
+console.log(nam_array);
+//map function
+
+let w_array = [45,76,55,35,97,45];
+w_array.map((value,index)=>{
+  console.log(value, index);
+})
+//filter function
+let arr23 = [12,76,454,34,5433,685];
+
+let a2 =arr23.filter((a)=>{
+  return a<10;
+  
+})
+console.log(a2);
