c6c6e86f1056f25d18067ab579feb1477929e9ba
Hashim khan <hashimkhanlazak0301@gmail.com>
2025-10-29 23:03:07 +0500

    today we practice on promises and loadscript and multiple handles

--- 3rd.js
+++ 3rd.js
@@ -52,11 +52,101 @@ function saybye(){
 }
 greet("hashim", saybye);
 //
-let promise = new Promise(function(resolve, reject){
-  console.log("the delay");
 
-  setTimeout(() => {
-    console.log("the delay is over.....");
-  }, 2000);
+let p = new Promise((resolve, reject)=>{
+  console.log("the promise is pending");
+
+setTimeout(()=>{
+  console.log("the settimeout is over");
+  resolve(true);
+}, 5000);
+})
+let p2 = new Promise((resolve, reject)=>{
+
+setTimeout(()=>{
+  console.log("the promise is rejected");
+  reject(false);
+
+},5000);
+
+})
+
+p.then((value) => {
+  console.log("the resolve.");
+})
+p2.catch((err) => {
+  console.log("the error occur due to the some reason");
+  
 });
-console.log(promise);
+
+
+// let p3 = new Promise((resolve, reject)=>{
+//   console.log("the promise is pending.....");
+
+//   setTimeout(()=>{
+//     console.log("the promise is resolve.");
+//     resolve(true);
+//   },3000)
+// })
+
+// let p4 = new Promise((resolve, reject)=>{
+//   console.log("the rejected error occur.");
+
+//   setTimeout(()=>{
+//     console.log("the settimeout is occur and out upto 3000");
+//     reject(false);
+//   },3000);
+// })
+// p3.then((value)=>{
+//    console.log("the then is over");
+// })
+// p4.catch((error)=>{
+//    console.log("the error occur due to the some mistake!!!");
+// })
+
+//we used the loadscript
+
+const loadScript = (src)=>{
+  return new Promise ((resolve, reject) =>{
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = src;
+  documnet.body.appendChild(script);
+  script.onload = ()=>{
+    resolve("script is loaded is sir...");
+  }
+  script.onerror = ()=>{
+    reject(0);
+  }
+})
+
+  
+}
+let p1 = loadScript("	https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js");
+p1.then((value)=>{
+  console.log(value);
+}).then((value)=>{
+ console.log("the value is this  print.");
+}).catch((error)=>{
+  console.log("some error occur because the error is not correct");
+})
+
+//we used the multiple handlers .
+let P = new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+       console.log("the settimeout is over under the 4000");
+        resolve(4)
+     },4000);
+
+    
+})
+
+P.then((value)=>{
+  console.log(value);
+})
+
+P.then((value)=>{
+  console.log("congradulation the resolve value is solved.");
+}).then((value)=>{
+  console.log("the harrry.");
+})
